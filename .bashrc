440e5435c4a158a0254cb2475c660cbf6ef67cfb
J. Macedo <jm.oliveira.jr@gmail.com>
2025-01-19 14:55:28 -0300

    Shell config (only)

--- .bashrc
+++
@@ -1,5 +0,0 @@
-if [ -f ~/.bash_functions ]; then 
-	. ~/.bash_functions
-fi
-
-export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]$(git_info)\[\033[01;34m\]\[\033[00m\]\n\$ '
--- post-install.sh
+++ post-install.sh
@@ -1,58 +1,20 @@
 #!/bin/bash
 
-DIR_HOME="/home/$USER"
+WELCOME_STRING="\n#============================================================"\
+"\n# Author: José Macedo"\
+"\n# Creation:19/01/2025"\
+"\n# Last Modify: 19/01/2025"\
+"\n# From this point on, the settings are made by the user."\
+"\n#============================================================\n";
 
-if [ -d $DIR_HOME ] && [ ! -f "$DIR_HOME/.gitconfig" ]; then
-    cat "./.bashrc" >> "$DIR_HOME/.bashrc"
-    cat "./.bash_functions" >> "$DIR_HOME/.bash_functions"
-    cp -f ".gitconfig" $DIR_HOME
-    cp -Rf "./.ssh" $DIR_HOME
-else
-    echo -e "Diretório de usuário '$DIR_HOME' não foi encontrado, ou os arquivos de configuração já foram escritos.\n"
-fi
+WELCOME_ALIASES_STRING="\n#============================================================"\
+"\n# ALIASES"\
+"\n#============================================================\n";
 
-echo -e "\n\n>> Adicionando repositórios de terceiros ao sources.list\n\n"
+ALIASES=`cat aliases.jm`;
 
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:linuxuprising/java
+# BASH
+echo -e "\n$WELCOME_STRING$WELCOME_ALIAS_STRING\n$ALIASES\n" >> ~/.bashrc;
 
-echo -e "\n\n>> Atualizando a lista de repositórios\n\n"
-sudo apt update
-
-echo -e "\n\n>> Excluindo programas indesejados\n\n"
-sudo apt -y purge thunderbird rhythmbox
-
-echo -e "\n\n>> Atualizando os programas pré-instalados\n\n"
-sudo apt -y upgrade
-
-echo -e "\n\n>> Atualizando o sistema operacional\n\n"
-sudo apt -y dist-upgrade
-
-echo -e "\n\n>> Instalando programas\n\n"
-sudo apt install -y \
-gparted \
-vlc \
-vim \
-unrar \
-terminator \
-snap \
-snapd
-
-echo -e "\n\n>> Instalando Java 15 (necessita intervenção humana):\n"
-sudo apt install oracle-java15-installer
-
-# Instalação via SNAP do Postman
-sudo snap install postman
-
-sudo apt -y autoremove
-sudo apt autoclean
-
-echo -e "\n\nProgramas instalados:\n"
-echo -e "- gparted\n"
-echo -e "- vlc\n"
-echo -e "- Postman\n"
-echo -e "- VIM\n"
-echo -e "- unrar\n"
-echo -e "- terminator\n"
-echo -e "- snap\n"
-echo -e "- Java 15\n"
+#ZSH
+echo -e "\n$WELCOME_STRING$WELCOME_ALIAS_STRING\n$ALIASES\n" >> ~/.zshrc;
